4e07c7544ba67fef4a23c820bd8e506e0a199d47
gofar09 <gordon.d.farrell@gmail.com>
2018-06-07 16:42:00 -0400

    Add first large commit, detailed below -Create 3x3 grid -Create first jquery event handlers for grid -Begin creation and testing for game logic TODO: Commit more often

---
+++ assets/scripts/events.js
@@ -0,0 +1,31 @@
+
+const authApi = require('./api')
+const ui = require('./ui')
+
+let gameLogic = [null, null, null, null, null, null, null, null, null]
+
+const boardReset = function (arr) {
+  for (let i = 0; i < gameLogic.length; i++) {
+    arr[i] = null
+  }
+}
+
+let xOrO = true
+console.log(xOrO)
+const turn = function () {
+  xOrO = !xOrO
+  console.log(xOrO)
+}
+
+const clickDisplay = function () {
+  gameLogic[i] = xOrO
+}
+
+const test = function () {
+  console.log('test successful')
+}
+
+module.exports = {
+  turn,
+  test
+}
--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 'use strict'
-
 // user require with a reference to bundle the file and use it in this file
 // var example = require('./example');
-
 // load manifests
 // scripts
 require('./assets/scripts/index.js')
-
+const events = require('./assets/scripts/events.js')
 // styles
 require('./assets/styles/index.scss')
+
+$(() => {
+  $('.game-box').on('click', events.turn)
+  $('#zero').on('click', events.test)
+  $('#one').on('click', events.test)
+  $('#two').on('click', events.test)
+  $('#three').on('click', events.test)
+  $('#four').on('click', events.test)
+  $('#five').on('click', events.test)
+  $('#six').on('click', events.test)
+  $('#seven').on('click', events.test)
+  $('#eight').on('click', events.test)
+})
