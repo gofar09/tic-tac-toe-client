bdb8494a9974d03987222edc158a75a00b72b884
gofar09 <gordon.d.farrell@gmail.com>
2018-06-08 15:34:34 -0400

    Add complete basics of Tic Tac Toe -Functional basic game -Functional new game button

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -4,28 +4,62 @@ const ui = require('./ui')
 
 let gameLogic = [null, null, null, null, null, null, null, null, null]
 
-const boardReset = function (arr) {
-  for (let i = 0; i < gameLogic.length; i++) {
-    arr[i] = null
+let ids = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight']
+
+let letter = true
+// console.log(letter)
+const changeTurn = function () {
+  letter = !letter
+}
+
+const which = () => {
+  if (letter === true) {
+    console.log(gameLogic)
+    return 'X'
+  } else {
+    return 'O'
   }
 }
 
-let xOrO = true
-// console.log(xOrO)
-const changeTurn = function () {
-  xOrO = !xOrO
-  console.log(xOrO)
+const xoPush = (id) => {
+  let holder = ids.indexOf(id)
+  console.log('holder is' + holder)
+  gameLogic[Number(holder)] = which()
+  console.log(gameLogic)
+}
+
+const boardWrite = () => {
+  $('#zero').text(gameLogic[0])
+  $('#one').text(gameLogic[1])
+  $('#two').text(gameLogic[2])
+  $('#three').text(gameLogic[3])
+  $('#four').text(gameLogic[4])
+  $('#five').text(gameLogic[5])
+  $('#six').text(gameLogic[6])
+  $('#seven').text(gameLogic[7])
+  $('#eight').text(gameLogic[8])
 }
 
 const clickDisplay = function () {
-  gameLogic[i] = xOrO
+  xoPush($(this).attr('id'))
+  boardWrite()
+}
+
+const boardReset = function () {
+  for (let i = 0; i < gameLogic.length; i++) {
+    gameLogic[i] = null
+    console.log(gameLogic)
+    boardWrite()
+  }
 }
 
 const test = function () {
-  console.log('test successful')
+  console.log($(this).attr('id'))
 }
 
 module.exports = {
   changeTurn,
-  test
+  test,
+  clickDisplay,
+  boardReset
 }
